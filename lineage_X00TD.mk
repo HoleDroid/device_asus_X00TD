2f03cb45c1eccddf0942ed2fad329eb6f7d1da82
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-20 16:59:50 +0530

    X00TD: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: I54ddc805b67a0bc91f0d2e60066e1f525cb77c96

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -10,7 +10,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
